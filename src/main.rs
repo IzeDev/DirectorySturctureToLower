6308b944980e7fdbb9714e9224ae2672428e251a
IzeDev <jimmy.gustafsson@live.com>
2022-01-05 18:52:58 +0100

    Made it work, but ugly.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
+use core::cmp::max;
 use std::{
     fs,
     io::{Error, ErrorKind},
 };
 
-fn get_subfolders(path: &str) -> Result<Vec<String>, Error> {
+fn get_subfolders(path: &str, level: i32) -> Result<Vec<(String, i32)>, Error> {
     if let Ok(entries) = fs::read_dir(path) {
         Ok(entries
             .filter_map(|e| e.ok())
-            .map(|e| e.path().display().to_string())
+            .map(|e| (e.path().display().to_string(), level))
             .collect())
     } else {
         Err(Error::new(
@@ -17,28 +18,36 @@ fn get_subfolders(path: &str) -> Result<Vec<String>, Error> {
     }
 }
 
-fn get_directory_tree(directory_tree: Vec<(String, i32)>) {
-    let mut number_of_directories = 1;
+fn get_directory_tree(mut directory_tree: Vec<(String, i32)>) ->Vec<(String, i32)> {
 
-    while number_of_directories < directory_tree.len() {
-        let current_level = 0;
+    loop {
+        let elements_at_start = directory_tree.len();
+        let current_max = directory_tree.iter().map(|d| d.1).max().unwrap();
 
-        let x = &directory_tree
-            .into_iter()
-            .filter_map(|d: (String, i32)| get_subfolders(&d.0).ok())
-            .fold(init: Acc, fold: Fold)
-            // .map(|d| (d, current_level + 1))
-            // .collect();
+        let mut sub_directories: Vec<(String, i32)> = directory_tree
+            .iter()
+            .filter(|d| d.1 == current_max)
+            .filter_map(|d: &(String, i32)| get_subfolders(&d.0, current_max + 1).ok())
+            .flatten()
+            .map(|d: (String, i32)| (d.0, d.1))
+            .collect();
+
+        let elements_at_end = directory_tree.len() + sub_directories.len();
+        directory_tree.append(&mut sub_directories);   
+        
+
+        if elements_at_end == elements_at_start {
+            break;
+        }
     }
+
+    directory_tree
 }
 
 fn main() {
-    if let Ok(sufolders) = get_subfolders("C:\\Users\\Jimmy\\Desktop\\Test") {
+    if let Ok(mut sufolders) = get_subfolders("/home/jimmy/Desktop/Test", 0) {
+        let subfolders = get_directory_tree(sufolders);
         println!("Success!");
-        for subfolder in &sufolders {
-            println!("{}", subfolder);
-        }
-        sufolders.into_iter().for_each(|f| println!("{}", f));
     } else {
         println!("Failure!");
     }
